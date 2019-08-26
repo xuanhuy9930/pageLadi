dafe5e3f2cbbd0aa91cb76e7112e4882a4ad309f
xuanhuy9930 <xuanhuy9930@gmail.com>
2019-08-26 09:40:28 +0700

    ...

---
+++ resources/views/ladipage.blade.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/
+	all.min.css">
+	<!-- <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet"> -->
+
+
+	<link rel="stylesheet" href="css/ladi.css">
+</head>
+<body>
+	<div id="page">
+		<div class="header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 header-ladipage">
+						<div class="header-content">
+							<img class="logo-header" src="image/logo-Ladypage.png" alt="">
+							<h2 class="title-ks">Căn hộ khách sạn LadiPage</h2>
+							<h1 class="view-header">3 mặt hồ tây</h1>
+							<h6 class="sl-pt">Sinh lời từ 12 - 15% mỗi năm <br>
+							Chỉ đóng 15% có ngay căn hộ</h6>
+							<div class="space"></div>
+							<h2 class="title-ks"><i class="fas fa-phone"></i>&ensp;0972 220 777</h2>
+							<a class="dangky" href="{{route('ladipage')}}">Đăng ký tư vấn</a>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="quangcao">
+			<div class="container">
+				<div class="list-qc row">
+					<div class="col-md-3 box-qc">
+						<i class="far fa-check-square i-list"></i>
+						<h6 class="title-box">KHÔNG GIAN SỐNG ĐẲNG CẤP</h6>
+						<p class="text-box">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+					</div>
+					<div class="col-md-3 box-qc">
+						<i class="fas fa-user-plus i-list"></i>
+						<h6 class="title-box">TÀI SẢN ĐẦU TƯ TIỀM NĂNG</h6>
+						<p class="text-box">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+					</div>
+					<div class="col-md-3 box-qc">
+						<i class="fas fa-tv i-list"></i>
+						<h6 class="title-box">CĂN HỘ SIÊU CAO CẤP</h6>
+						<p class="text-box">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+					</div>
+					<div class="col-md-3 box-qc">
+						<i class="fas fa-key i-list"></i>
+						<h6 class="title-box">AN NINH BIỆT LẬP</h6>
+						<p class="text-box">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="hinh-anh-du-an">
+			<div class="text-ha text-center">
+				<h2 class="text-title">Hình ảnh dự án</h2>
+				<p class="text-box">Lorem ipsum dolor sit amet, ea pro error pertinacia, nam id munere detraxit. Wisi decore discere eum an, autem <br> erant usu eu, per ubique legendos intellegam in. An est melius vivendo.</p>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3"><img class="img-hinh-anh" src="image/hinh-anh-content.jpg" alt=""></div>
+					<div class="col-md-3"><img class="img-hinh-anh" src="image/hinh-anh-content.jpg" alt=""></div>
+					<div class="col-md-3"><img class="img-hinh-anh" src="image/hinh-anh-content.jpg" alt=""></div>
+					<div class="col-md-3"><img class="img-hinh-anh" src="image/hinh-anh-content.jpg" alt=""></div>
+					<div class="col-md-3"><img class="img-hinh-anh" src="image/hinh-anh-content.jpg" alt=""></div>
+					<div class="col-md-3"><img class="img-hinh-anh" src="image/hinh-anh-content.jpg" alt=""></div>
+					<div class="col-md-3"><img class="img-hinh-anh" src="image/hinh-anh-content.jpg" alt=""></div>
+					<div class="col-md-3"><img class="img-hinh-anh" src="image/hinh-anh-content.jpg" alt=""></div>
+					
+				</div>
+			</div>
+		</div>
+
+		<div class="thong-tin-du-an">
+			<div class="text-ha text-center">
+				<h2 class="text-title">Hình ảnh dự án</h2>
+				<p class="text-box">Lorem ipsum dolor sit amet, ea pro error pertinacia, nam id munere detraxit. Wisi decore discere eum an, autem <br> erant usu eu, per ubique legendos intellegam in. An est melius vivendo.</p>
+			</div>
+			<div class="container text-center qc-thong-tin">
+				<div class="row">
+					<div class="col">
+						<i class="fas fa-camera i-cr"></i>
+						<h6 class="text-view">view đẹp</h6>
+						<h2 class="number">02</h2>
+					</div>
+					<div class="col">
+						<i class="fas fa-bed i-cr"></i>
+						<h6 class="text-view">view đẹp</h6>
+						<h2 class="number">03</h2>
+					</div>
+					<div class="col">
+						<i class="fas fa-warehouse i-cr"></i>
+						<h6 class="text-view">view đẹp</h6>
+						<h2 class="number">01</h2>
+					</div>
+					<div class="col">
+						<i class="fas fa-gift i-cr"></i>
+						<h6 class="text-view">view đẹp</h6>
+						<h2 class="number">01</h2>
+					</div>
+					<div class="col">
+						<i class="fas fa-user-friends i-cr"></i>
+						<h6 class="text-view">view đẹp</h6>
+						<h2 class="number">1992</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="gioi-thieu-du-an">
+			<img class="img-du-an" src="image/gioi-thieu.jpg" alt="">
+			<div class="list-du-an">
+				<h3 class="title-du-an">Giới thiệu dự án</h3>
+				<p class="text-du-an">Mô tả thông tin có giá trị về sản phẩm dịch vụ, giúp người xem <br>dễ dàng tìm thấy nội dung của bạn trong kết quả tìm kiếm. <br> Những mô tả được viết tốt kèm theo các từ khóa phù hợp có thể <br> tăng lượt xem vì chúng hiển thị tốt trong kết quả tìm kiếm.</p>
+				<div class="space"></div>
+				<p class="text-du-an">Mô tả thông tin có giá trị, những điểm nổi bật, lời giới thiệu hấp <br> dẫn về sản phẩm dịch vụ, giúp người xem dễ dàng tìm thấy nội <br> dung của bạn trong kết quả tìm kiếm. Những mô tả được viết tốt <br> kèm theo các từ khóa phù hợp có thể tăng lượt xem vì chúng <br> giúp landingpage của bạn hiển thị tốt trong kết quả tìm kiếm.</p>
+			</div>
+		</div>
+
+		<div class="lua-chon">
+			<div class="text-ha text-center">
+				<h2 class="text-title">Hình ảnh dự án</h2>
+				<p class="text-box">Lorem ipsum dolor sit amet, ea pro error pertinacia, nam id munere detraxiat. Wisi decore discere eum an, autem <br> erant usu eu, per ubique legendos intellegam in. An est melius vivendo.</p>
+			</div>
+
+			<div class="img-biet-thu container">
+				<div class="row">
+					<div class="col-md-3 box-biet-thu">
+						<img class="image-box" src="image/biet-thu.jpg" alt="">
+						<h6 class="title-biet-thu">biệt thự 01</h6>
+						<p class="text-biet-thu">Là tổ hợp gồm các toà nàh kết hợp hoàn hảo giữa phong cách kiến trúc hiện đại, cảnh quan xanh, trình độ quản lý chuyên nghiệp, dịch vụ và tiện ích cao cấp mang lại cho cư dân một không gian sống lý tưởng.</p>	<div class="dollar">				
+							<h5 class="dollar-icon"><i class="fas fa-paperclip"></i>&nbsp; $250,000</h5>
+							<h5 class="visited-icon"><i class="far fa-flag"></i>&nbsp;Hoi An, Da Nang</h5>
+							<h5 class="home-icon"><i class="fas fa-home"></i>&nbsp;3 bedrooms, 1 livingroom</h5>
+						</div>
+					</div>
+					<div class="col-md-3 box-biet-thu">
+						<img class="image-box" src="image/biet-thu.jpg" alt="">
+						<h6 class="title-biet-thu">biệt thự 01</h6>
+						<p class="text-biet-thu">Là tổ hợp gồm các toà nàh kết hợp hoàn hảo giữa phong cách kiến trúc hiện đại, cảnh quan xanh, trình độ quản lý chuyên nghiệp, dịch vụ và tiện ích cao cấp mang lại cho cư dân một không gian sống lý tưởng.</p>	<div class="dollar">				
+							<h5 class="dollar-icon"><i class="fas fa-paperclip"></i>&nbsp; $250,000</h5>
+							<h5 class="visited-icon"><i class="far fa-flag"></i>&nbsp;Hoi An, Da Nang</h5>
+							<h5 class="home-icon"><i class="fas fa-home"></i>&nbsp;3 bedrooms, 1 livingroom</h5>
+						</div>
+					</div>
+					<div class="col-md-3 box-biet-thu">
+						<img class="image-box" src="image/biet-thu.jpg" alt="">
+						<h6 class="title-biet-thu">biệt thự 01</h6>
+						<p class="text-biet-thu">Là tổ hợp gồm các toà nàh kết hợp hoàn hảo giữa phong cách kiến trúc hiện đại, cảnh quan xanh, trình độ quản lý chuyên nghiệp, dịch vụ và tiện ích cao cấp mang lại cho cư dân một không gian sống lý tưởng.</p>	<div class="dollar">				
+							<h5 class="dollar-icon"><i class="fas fa-paperclip"></i>&nbsp; $250,000</h5>
+							<h5 class="visited-icon"><i class="far fa-flag"></i>&nbsp;Hoi An, Da Nang</h5>
+							<h5 class="home-icon"><i class="fas fa-home"></i>&nbsp;3 bedrooms, 1 livingroom</h5>
+						</div>
+					</div>
+					<div class="col-md-3 box-biet-thu">
+						<img class="image-box" src="image/biet-thu.jpg" alt="">
+						<h6 class="title-biet-thu">biệt thự 01</h6>
+						<p class="text-biet-thu">Là tổ hợp gồm các toà nàh kết hợp hoàn hảo giữa phong cách kiến trúc hiện đại, cảnh quan xanh, trình độ quản lý chuyên nghiệp, dịch vụ và tiện ích cao cấp mang lại cho cư dân một không gian sống lý tưởng.</p>	<div class="dollar">				
+							<h5 class="dollar-icon"><i class="fas fa-paperclip"></i>&nbsp; $250,000</h5>
+							<h5 class="visited-icon"><i class="far fa-flag"></i>&nbsp;Hoi An, Da Nang</h5>
+							<h5 class="home-icon"><i class="fas fa-home"></i>&nbsp;3 bedrooms, 1 livingroom</h5>
+						</div>
+					</div>
+					<div class="col-md-3 box-biet-thu">
+						<img class="image-box" src="image/biet-thu.jpg" alt="">
+						<h6 class="title-biet-thu">biệt thự 01</h6>
+						<p class="text-biet-thu">Là tổ hợp gồm các toà nàh kết hợp hoàn hảo giữa phong cách kiến trúc hiện đại, cảnh quan xanh, trình độ quản lý chuyên nghiệp, dịch vụ và tiện ích cao cấp mang lại cho cư dân một không gian sống lý tưởng.</p>	<div class="dollar">				
+							<h5 class="dollar-icon"><i class="fas fa-paperclip"></i>&nbsp; $250,000</h5>
+							<h5 class="visited-icon"><i class="far fa-flag"></i>&nbsp;Hoi An, Da Nang</h5>
+							<h5 class="home-icon"><i class="fas fa-home"></i>&nbsp;3 bedrooms, 1 livingroom</h5>
+						</div>
+					</div>
+					<div class="col-md-3 box-biet-thu">
+						<img class="image-box" src="image/biet-thu.jpg" alt="">
+						<h6 class="title-biet-thu">biệt thự 01</h6>
+						<p class="text-biet-thu">Là tổ hợp gồm các toà nàh kết hợp hoàn hảo giữa phong cách kiến trúc hiện đại, cảnh quan xanh, trình độ quản lý chuyên nghiệp, dịch vụ và tiện ích cao cấp mang lại cho cư dân một không gian sống lý tưởng.</p>	<div class="dollar">				
+							<h5 class="dollar-icon"><i class="fas fa-paperclip"></i>&nbsp; $250,000</h5>
+							<h5 class="visited-icon"><i class="far fa-flag"></i>&nbsp;Hoi An, Da Nang</h5>
+							<h5 class="home-icon"><i class="fas fa-home"></i>&nbsp;3 bedrooms, 1 livingroom</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="tu-van">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 nv">
+						<img class="nhan-vien" src="image/nhan-vien-tu-van.png" alt="">
+					</div>
+					<div class="col-md-6 tu-van-item">
+						<h3 class="title-tu-van">Bạn muốn được tư vấn về dự án?</h3>
+						<p class="text-box">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod <br> tempor incididunt ut labore et dolore magna aliqua</p>
+						<div class="space"></div>
+						<p class="text-tu-van">Liên hệ để nắm rõ thông tin tốt nhất</p>
+						<div class="row add-thongtin">
+							<div class="col-md-6">
+								<input class="hoten" type="text" placeholder="Họ và Tên">
+								<input class="hoten" type="text" placeholder="Nhập Email">
+							</div>
+							<div class="col-md-6 add-phone">
+								<input class="hoten" type="text" placeholder="Nhập số điện thoại">
+								<a class="send" href="">Gửi Đi</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="hot-line">
+			<div class="hot-line-ho-tro">
+				<h6 class="ho-tro">Hỗ trợ khách hàng 24/24</h6>
+				<span class="sdt-hot">Hotline: 0972 220 777</span>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<div class="footer">
+			<div class="container">
+				<div class="row list-bot">
+					<div class="col-md-6 ">
+						<img class="logo-header" src="image/logo-Ladypage.png" alt="">
+						<p class="visit-footer">Địa chỉ văn phòng <br> Tầng 6A, số 9A, ngõ 9, Hoàng Cầu, Đống Đa, Hà Nội</p>
+					</div>
+					<div class="col-md-6 dang-nhap-emal">
+						<p class="dangnhap">Đăng nhập để nhận các thông tin, ưu đãi mới nhất</p>
+						<input class="nhap-email" type="text" placeholder="Nhập email"><a class="gui-di" href="">Gửi Đi</a> <br>
+						<i class="fab fa-facebook-f icon-bot"></i>
+						<i class="fab fa-youtube icon-bot"></i>
+					</div>
+					<div class="space"></div>
+					<p class="kt">Ladipage - Landing pages © 2017</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
